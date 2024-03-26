d873ae1e4bad33bc7b7dd770bffc468853970dbc
Lionel TOTON <totonlionel@gmail.com>
2024-03-26 12:35:37 +0100

    Départ de Projet

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const app = require('express');
+const express = require('express');
+
+const path = require("path");
+
+let initial_path = path.join(__dirname, 'frontend');
+
+const app = express();
+
+app.use(express.static(initial_path));
+
+const port = 4000;
+
+app.listen(port, () => {
+    console.log(`The server is running on: http://localhost:${port}`)
+})
